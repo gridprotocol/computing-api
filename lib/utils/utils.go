f813b20b51a7b54e315030ddcbd7021ddf67bb68
rocky <rocky@memoio.io>
2025-01-12 13:06:01 +0800

    clean

--- lib/utils/utils.go
+++ lib/utils/utils.go
@@ -134,13 +134,11 @@ func SendGetOrderRequest(id uint64) (*Order, error) {
 	defer resp.Body.Close()
 
 	// 读取响应内容
-	fmt.Println("reading response")
+
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, fmt.Errorf("error reading response body: %w", err)
 	}
-
-	fmt.Printf("response body: %x", body)
 
 	// 解析响应内容为 Order 结构体
 	var order Order
