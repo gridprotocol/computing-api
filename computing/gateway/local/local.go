07a0770c5d9348fc22e5fb01409b3907e5b9dda3
rocky <rocky@memoio.io>
2025-01-12 17:45:25 +0800

    fix

--- computing/gateway/local/local.go
+++ computing/gateway/local/local.go
@@ -137,7 +137,7 @@ func (glp *GatewayLocalProcess) Deploy(deps []*appsv1.Deployment, svcs []*corev1
 
 	user_oid := fmt.Sprintf("%s-%d", user, oid)
 	key := prefixKey(entrancePrefix, user_oid)
-	fmt.Printf("key: %s", key)
+	fmt.Printf("key for entrance: %s\n", key)
 	// record entrance
 	err = glp.DB.Put(key, []byte(entrance))
 	if err != nil {
