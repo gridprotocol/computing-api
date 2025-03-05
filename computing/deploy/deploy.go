37ab852b015a076be361b1cb17826c1aac1104af
rocky <rocky@memoio.io>
2025-03-05 18:15:18 +0800

    auto rm memo files with prestop in yaml

--- computing/deploy/deploy.go
+++ computing/deploy/deploy.go
@@ -3,9 +3,6 @@ package deploy
 import (
 	"context"
 	"fmt"
-	"os"
-	"os/user"
-	"path/filepath"
 	"strings"
 	"time"
 
@@ -339,28 +336,6 @@ func DelDeploy(depName string) error {
 		return err
 	}
 
-	// remove dir for mefs-user
-	if strings.HasPrefix(depName, "mefs-user") {
-		// 删除文件夹
-		err := deleteFolders("mefs-user")
-		if err != nil {
-			fmt.Println("mefs-user 删除文件夹失败:", err)
-		} else {
-			fmt.Println("mefs-user 文件夹删除成功")
-		}
-	}
-
-	// remove dir for mefs-provider
-	if strings.HasPrefix(depName, "mefs-provider") {
-		// 删除文件夹
-		err := deleteFolders("mefs-provider")
-		if err != nil {
-			fmt.Println("mefs-provider 删除文件夹失败:", err)
-		} else {
-			fmt.Println("mefs-provider 文件夹删除成功")
-		}
-	}
-
 	// delete svc
 	logger.Info("delete svc: ", "svc-", depName)
 	err = k8s.DeleteService(context.Background(), "default", fmt.Sprintf("svc-%s", depName))
@@ -378,46 +353,6 @@ func Clean(deps []*appsv1.Deployment) error {
 		err := DelDeploy(dep.Name)
 		return err
 	}
-
-	return nil
-}
-
-// 删除 ~/memo_user 和 ~/memo_user_data 文件夹
-func deleteFolders(role string) error {
-	// 获取当前用户的主目录
-	usr, err := user.Current()
-	if err != nil {
-		return fmt.Errorf("获取用户主目录失败: %v", err)
-	}
-
-	fmt.Println("current user::", usr)
-
-	// 构建文件夹路径
-	var folder1 string
-	var folder2 string
-
-	switch role {
-	case "mefs-user":
-		folder1 = filepath.Join(usr.HomeDir, "memo_user")
-		folder2 = filepath.Join(usr.HomeDir, "memo_user_data")
-	case "mefs-provider":
-		folder1 = filepath.Join(usr.HomeDir, "memo_provider")
-		folder2 = filepath.Join(usr.HomeDir, "memo_provider_data")
-	}
-
-	// 删除第一个文件夹
-	err = os.RemoveAll(folder1)
-	if err != nil {
-		return fmt.Errorf("rm dir %s failed: %v", folder1, err)
-	}
-	fmt.Printf("dir removed: %s\n", folder1)
-
-	// 删除第二个文件夹
-	err = os.RemoveAll(folder2)
-	if err != nil {
-		return fmt.Errorf("rm dir %s failed: %v", folder2, err)
-	}
-	fmt.Printf("dir removed: %s\n", folder2)
 
 	return nil
 }
